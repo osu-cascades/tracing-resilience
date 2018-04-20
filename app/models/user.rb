2d575cfdc69a6d82a887d7a79b61a8b997f49265
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-20 13:40:46 -0700

    Ref #5, configure signup check box agreement of terms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :terms_of_service
+  validates_acceptance_of :terms_of_service, accept: true, on: :create
 end
