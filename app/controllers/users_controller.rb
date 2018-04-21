9962065a9412f1b53ccd2664d18f8748e03e7c48
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-21 14:33:32 -0700

    Ref #9, enable admins to delete users and make guests admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,27 @@ class UsersController < ApplicationController
     @guests = User.where(role: :guest)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+      redirect_to users_path
+      flash[:success] = 'User successfully deleted.'
+    end
+  end
+
+  def make_admin
+    @user  = User.find(params[:id])
+    @user.role = :admin
+    @user.save
+
+    if @user.save
+      redirect_to users_path
+      flash[:success] = "#{@user} is now an administrator."
+    end
+  end
+
   private
 
   def require_admin
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'static#home'
   devise_for :users, controllers: { registrations: 'registrations' }
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+  match 'users/:id' => 'users#make_admin', :via => :get, :as => :make_admin
   resources :users, only: [:index]
 end
