7abcd84f178781bf7f5391aa87136368581b27cb
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-21 12:05:08 -0700

    Remove failing acceptance validation for terms of service will log as bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,10 +4,13 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email,
-      :organization, :city, :state, :zip, :phone, :password, :password_confirmation)
+      :organization, :city, :state, :zip, :phone, :password, :password_confirmation,
+      :terms_of_service)
   end
 
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email,
-      :organization, :city, :state, :zip, :phone, :password, :password_confirmation)
+      :organization, :city, :state, :zip, :phone, :password, :password_confirmation,
+      :terms_of_service)
+  end
 end
