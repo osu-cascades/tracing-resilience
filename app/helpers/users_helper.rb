536c55d9a3ce9ed834c1a210d00f2a4087cbfe0c
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-24 18:20:03 -0700

    Clean up user index formatting

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
   def delete_user_link(current_user, user)
     if current_user.admin? && current_user != user
-      "| #{link_to "delete", admin_destroy_user_path(user), method: :delete, data: { confirm: "Delete #{user}?" }, class: "lnk-danger", title: "Delete #{user}"}".html_safe
+      "| #{link_to "Delete", admin_destroy_user_path(user), method: :delete, data: { confirm: "Delete #{user}?" }, class: "lnk-danger", title: "Delete #{user}"}".html_safe
     end
   end
 
   def make_user_admin_link(current_user, user)
     if current_user.admin? && current_user != user
-       "| #{link_to "Make Admin", make_admin_path(user),  method: :get, data: { confirm: "Make #{user} an adminstrator?" }}".html_safe
+       "| #{link_to "Enable admin privileges", make_admin_path(user),  method: :get, data: { confirm: "Make #{user} an adminstrator?" }}".html_safe
     end
   end
 end
