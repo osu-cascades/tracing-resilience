de83faac4a3cf83656b95db84ab4a94dbb63ef58
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-26 11:29:07 -0700

    Configure factories for each category of measure

--- spec/factories/measures.rb
+++ spec/factories/measures.rb
@@ -1,6 +1,19 @@
 FactoryBot.define do
-  factory :measure do
-    title "MyString"
-    category 1
+  factory :general_measure do
+    title "Example Measure Title"
+    category 0
+    document ''
+  end
+
+  factory :individual_measure, parent: :general_measure do
+    role 1
+  end
+
+  factory :relational_measure, parent: :general_measure do
+    role 2
+  end
+
+  factory :community_measure, parent: :general_measure do
+    role 4
   end
 end
