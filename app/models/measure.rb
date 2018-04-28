1abe8ac9fb5f51d9f401e2b261675823045c4acd
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-28 11:22:10 -0700

    Add title and category to measure form

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -1,4 +1,13 @@
 class Measure < ApplicationRecord
   enum category: [:general, :individual, :relational, :community]
+
+  def self.category_options
+    options = {}
+    Measure.categories.keys.each do |k|
+      options[k.to_s.capitalize] = k
+    end
+    options
+  end
+
   validates_presence_of :title, :category
 end
