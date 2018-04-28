5092efdf31070d3fe9ea8d5d8aa52adef5654134
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-28 09:59:44 -0700

    Minor tweak to model spec

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Measure, type: :model do
   describe 'validations' do
-    [:title, :category].each do |a|
-      it { is_expected.to validate_presence_of(a) }
+    [:title, :category].each do |attribute|
+      it { is_expected.to validate_presence_of(attribute) }
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe User, type: :model do
   subject(:user) { build(:user) }
 
   describe 'validations' do
-    [:role, :first_name, :last_name, :organization, :city, :state, :zip].each do |a|
-      it { is_expected.to validate_presence_of(a) }
+    [:role, :first_name, :last_name, :organization, :city, :state, :zip].each do |attribute|
+      it { is_expected.to validate_presence_of(attribute) }
     end
 
     # it { is_expected.to validate_acceptance_of(:terms_of_service)}
