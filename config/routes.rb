34f575300b8355dfc01dd77021cb20147e10fbe7
Nathan Struhs <nathanstruhs@gmail.com>
2018-04-28 12:32:43 -0700

    Minor formatting and semantics

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root 'static#home'
   devise_for :users, controllers: { registrations: 'registrations' }
-  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
-  match 'users/:id' => 'users#make_admin', :via => :get, :as => :make_admin
+  match 'users/:id' => 'users#destroy', via: :delete, as: :admin_destroy_user
+  match 'users/:id' => 'users#make_admin', via: :get, as: :make_admin
   resources :users, only: [:index]
   resources :measures do
     collection do
-      match :general, :via => :get
-      match :individual, :via => :get
-      match :relational, :via => :get
-      match :community, :via => :get
+      match :general,    via: :get
+      match :individual, via: :get
+      match :relational, via: :get
+      match :community,  via: :get
     end
   end
 end
