19c07e3306faa71c9c5d9679c8e9e4a80b26124d
Yong Bakos <ybakos@humanoriented.com>
2018-05-17 23:14:24 -0700

    Delete route tests. The combination of system, controller and request tests will supercede these.

--- spec/routing/user_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe UsersController, type: :routing do
-  describe 'routes users pages' do
-    it 'routes to #index' do
-      expect(get: '/users').to route_to(controller: 'users', action: 'index')
-    end
-  end
-end
