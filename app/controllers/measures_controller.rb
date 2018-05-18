2b14fcf3d331f29eb429dd95ff21b59b221fd35e
Yong Bakos <ybakos@humanoriented.com>
2018-05-17 23:24:04 -0700

    MeasuresController: Add before_filter requiring admin before edit, delete, etc.

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,6 +1,7 @@
 class MeasuresController < ApplicationController
-  before_action :set_measure, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :require_admin, except: [:index, :show]
+  before_action :set_measure, only: [:show, :edit, :update, :destroy]
 
   def index
     @measures = Measure.all
