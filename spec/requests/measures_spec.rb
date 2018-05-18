39173ead37b6a3cfce62a17b6c5c96460e771ed4
Yong Bakos <ybakos@humanoriented.com>
2018-05-17 23:17:09 -0700

    Requests: Stub user and measure request specs.
    To verify access control for logged in and non-logged in users, let's use
    request specs to verify response status codes. For example, a user who is
    not logged in and requests /measures should receive a redirect, not a 200.

---
+++ spec/requests/measures_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Measures requests" do
+
+  context "when not logged in" do
+
+  end
+
+  context "when logged in as a registered user" do
+
+  end
+
+  context "when logged in as an admin" do
+
+  end
+
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Users requests" do
+
+  context "when not logged in" do
+
+  end
+
+  context "when logged in as a registered user" do
+
+  end
+
+  context "when logged in as an admin" do
+
+  end
+
+end
