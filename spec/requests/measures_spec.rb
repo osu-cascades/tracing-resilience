429110c9499268481be505f9cf99a53a1cd9c57b
Yong Bakos <ybakos@humanoriented.com>
2018-05-18 00:37:33 -0700

    Requests: Test requests and responses for non-logged-in users.

--- spec/requests/measures_spec.rb
+++ spec/requests/measures_spec.rb
@@ -2,16 +2,53 @@ require 'rails_helper'
 
 RSpec.describe "Measures requests" do
 
-  context "when not logged in" do
+  let(:measure) { create(:individual_measure) }
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
 
+  context "when not logged in" do
+    it "redirects to the sign in path" do
+      get measures_path
+      expect(response).to redirect_to(new_user_session_path)
+      get general_measures_path
+      expect(response).to redirect_to(new_user_session_path)
+      get individual_measures_path
+      expect(response).to redirect_to(new_user_session_path)
+      get relational_measures_path
+      expect(response).to redirect_to(new_user_session_path)
+      get community_measures_path
+      expect(response).to redirect_to(new_user_session_path)
+      get measure_path(measure)
+      expect(response).to redirect_to(new_user_session_path)
+      get new_measure_path
+      expect(response).to redirect_to(new_user_session_path)
+      get edit_measure_path(measure)
+      expect(response).to redirect_to(new_user_session_path)
+      post measures_path
+      expect(response).to redirect_to(new_user_session_path)
+      patch measure_path(measure)
+      expect(response).to redirect_to(new_user_session_path)
+      put measure_path(measure)
+      expect(response).to redirect_to(new_user_session_path)
+      delete measure_path(measure)
+      expect(response).to redirect_to(new_user_session_path)
+    end
   end
 
   context "when logged in as a registered user" do
+    skip "allows listing and viewing" do
 
+    end
+
+    skip "redirects to root url" do
+
+    end
   end
 
   context "when logged in as an admin" do
+    skip "responds to everything" do
 
+    end
   end
 
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "Users requests" do
 
-  context "when not logged in" do
+  let(:user) { create(:user) }
+  let(:admin) { create(:admin) }
 
+  context "when not logged in" do
+    it "redirects to the sign in path" do
+      get users_path
+      expect(response).to redirect_to(new_user_session_path)
+      get user_path(user)
+      expect(response).to redirect_to(new_user_session_path)
+      get edit_user_path(user)
+      expect(response).to redirect_to(new_user_session_path)
+      patch user_path(user)
+      expect(response).to redirect_to(new_user_session_path)
+      put user_path(user)
+      expect(response).to redirect_to(new_user_session_path)
+      delete user_path(user)
+      expect(response).to redirect_to(new_user_session_path)
+    end
   end
 
   context "when logged in as a registered user" do
+    skip "allows listing and viewing" do
 
+    end
+
+    skip "redirects to root url" do
+
+    end
   end
 
   context "when logged in as an admin" do
+    skip "responds to everything" do
 
+    end
   end
 
 end
