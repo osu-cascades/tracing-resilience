af7b8944a680e534769f51a9e93f6492c3b11c1d
Yong Bakos <ybakos@humanoriented.com>
2018-05-18 00:37:56 -0700

    MeasuresController: Remove authentication exceptions.

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,5 +1,5 @@
 class MeasuresController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
   before_action :require_admin, except: [:index, :show]
   before_action :set_measure, only: [:show, :edit, :update, :destroy]
 
