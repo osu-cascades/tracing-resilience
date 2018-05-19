e63587c3d10864fb4dad2f7502bed9ed5b6c6033
Nathan Struhs <nathanstruhs@gmail.com>
2018-05-19 15:11:24 -0700

    Refactor require_admin callback

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -1,6 +1,6 @@
 class MeasuresController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_admin, except: [:index, :show, :general, :individual, :relational, :community, :suggestion]
+  before_action :require_admin, only: [:new, :edit, :update, :destroy]
   before_action :set_measure, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -41,7 +41,7 @@ class MeasuresController < ApplicationController
       flash[:success] = 'Measure was successfully created.'
     else
       render :new
-      end
+    end
   end
 
   def update
