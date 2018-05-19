7b05fadd1cf2fd57d6e876b56860ad94685f0492
Yong Bakos <ybakos@humanoriented.com>
2018-05-19 15:47:33 -0700

    MAJOR: Upgrade Rails to 5.2.0. - Update Gemfile rails version, and add bootsnap gem. - Update configuration and add new config per rails app:update - Review, preseve and merge all config. - Add content_security_policy, storage.yml config, and new framework defaults (commented). References #27.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,18 +5,20 @@ Rails.application.configure do
 
   config.consider_all_requests_local = true
 
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
+
+  config.active_storage.service = :local
 
   config.action_mailer.raise_delivery_errors = false
 
@@ -28,11 +30,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'https://traces-cyrm-staging.herokuapp.com/' }
 
+  config.active_record.verbose_query_logs = true
+
   config.assets.debug = true
 
   config.assets.quiet = true
 
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.action_view.raise_on_missing_translations = true
 
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
