87ae8b470d6c2102c5a4453b5c823d71f59c1ab9
Yong Bakos <ybakos@humanoriented.com>
2018-05-31 20:16:06 -0600

    views/measures: Style list, remove partial. The individual templates per category will go away, so extract the list rendering out of the partial and put the duplication smell in the template.

--- app/helpers/measure_helper.rb
+++ app/helpers/measure_helper.rb
@@ -1,9 +1,3 @@
 module MeasureHelper
-  def show_measure_links(measure)
-    if current_user.admin?
-      "#{link_to measure.title, measure} - #{link_to 'Edit', edit_measure_path(measure)} | #{link_to 'Destroy', measure, method: :delete, data: { confirm: 'Are you sure?' }}".html_safe
-    else
-      "#{link_to measure.title, measure}".html_safe
-    end
-  end
+
 end
