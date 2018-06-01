1835983b044f856443f0604d8b8d3ed8b4c18972
Yong Bakos <ybakos@humanoriented.com>
2018-06-01 02:40:08 -0400

    User: Enable terms_of_use validation test. Test to_s.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe User, type: :model do
     [:role, :first_name, :last_name, :organization, :city, :state, :zip].each do |attribute|
       it { is_expected.to validate_presence_of(attribute) }
     end
+    it { is_expected.to validate_acceptance_of(:terms_of_use) }
+  end
 
-    # it { is_expected.to validate_acceptance_of(:terms_of_service)}
+  it 'has a string representation consisting of first and last name' do
+    expect(user.to_s).to eq "#{user.first_name} #{user.last_name}"
   end
 end
