80be3d45afc2d10b59b372536a70e02a49c78f7b
Yong Bakos <ybakos@humanoriented.com>
2018-06-01 03:01:23 -0400

    Measure: Test ::category_options.

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Measure, type: :model do
+
   describe 'validations' do
     [:title, :category].each do |attribute|
       it { is_expected.to validate_presence_of(attribute) }
     end
   end
+
+  describe '::category_options' do
+    it "returns a Hash of Name => name pairs" do
+      category_hash = {
+        "General"=>"general",
+        "Individual"=>"individual",
+        "Relational"=>"relational",
+        "Community"=>"community"
+      }
+      expect(Measure.category_options).to eq(category_hash)
+    end
+  end
+
 end
