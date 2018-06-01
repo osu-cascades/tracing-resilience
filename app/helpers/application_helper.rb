ca2fbbfc470ca7cdde130cb67c56105e3d23caee
Yong Bakos <ybakos@humanoriented.com>
2018-05-31 20:25:35 -0600

    Application: Change BASE_TITLE and test.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
-  BASE_TITLE = 'Traces CYRM'.freeze
+
+  BASE_TITLE = 'TRACES Resilience Measures'.freeze
+
   def full_title(page_title = '')
     page_title.empty? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
   end
+
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#full_title' do
     it 'returns the base title without arguments' do
-      expect(helper.full_title).to eq('Traces CYRM')
+      expect(helper.full_title).to eq('TRACES Resilience Measures')
     end
 
     it 'returns the page title and screen title with page argument' do
-      expect(helper.full_title('Home')).to eq('Home | Traces CYRM')
+      expect(helper.full_title('FAKE')).to eq('FAKE | TRACES Resilience Measures')
     end
   end
 end
