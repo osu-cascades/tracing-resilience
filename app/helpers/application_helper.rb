77d0d5f2d4fcb95d898cc1fb32bd60cb91b5a24c
Nathan Struhs <nathanstruhs@gmail.com>
2018-06-02 15:07:26 -0700

    Fixed #32, display errors user edit form Extract form errors into partial and helper Update user form as well

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
     page_title.empty? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
   end
 
+  def form_errors_for(object)
+    render partial: 'shared/form_errors', locals: { object: object }
+  end
+
 end
