5c4f62353ffb370e794f2473307d657c8c5a4974
Nathan Struhs <nathanstruhs@gmail.com>
2018-06-02 13:24:29 -0700

    Fixed #35, display count of measures per category

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -10,18 +10,22 @@ class MeasuresController < ApplicationController
 
   def general
     @measures = Measure.where(category: :general)
+    @measure_count = @measures.size
   end
 
   def individual
     @measures = Measure.where(category: :individual)
+    @measure_count = @measures.size
   end
 
   def relational
     @measures = Measure.where(category: :relational)
+    @measure_count = @measures.size
   end
 
   def community
     @measures = Measure.where(category: :community)
+    @measure_count = @measures.size
   end
 
   def show; end
--- app/helpers/measures_helper.rb
+++ app/helpers/measures_helper.rb
@@ -1,7 +1,15 @@
 module MeasuresHelper
+
   def show_new_measure_link_if_admin
     if current_user.admin?
       "#{link_to 'Add a new measure', new_measure_path, class: 'btn btn-primary float-right'}".html_safe
     end
+  end
+
+  def show_measure_count
+    count = @measure_count || 0
+    measure_count = "#{count} Measure".html_safe
+    unless count == 1 then measure_count << 's' end
+    measure_count.html_safe
   end
 end
