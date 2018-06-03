59f1a9ed1ecd957c4cae3f8ab0c714569bb0109c
Yong Bakos <ybakos@humanoriented.com>
2018-06-03 19:20:22 -0400

    MeasureSpec: Use single ticks.

--- spec/models/measure_spec.rb
+++ spec/models/measure_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe Measure, type: :model do
   end
 
   describe '::category_options' do
-    it "returns a Hash of Name => name pairs" do
+    it 'returns a Hash of Name => name pairs' do
       category_hash = {
-        "General"=>"general",
-        "Individual"=>"individual",
-        "Relational"=>"relational",
-        "Community"=>"community"
+        'General' => 'general',
+        'Individual' => 'individual',
+        'Relational' => 'relational',
+        'Community' => 'community'
       }
       expect(Measure.category_options).to eq(category_hash)
     end
