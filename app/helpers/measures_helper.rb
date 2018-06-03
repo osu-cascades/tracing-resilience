f7f8c9a781001ab9b5c9c12e813a6b6f43d7e05a
Nathan Struhs <nathanstruhs@gmail.com>
2018-06-03 15:27:11 -0700

    Measures: refactor new measure link helper

--- app/helpers/measures_helper.rb
+++ app/helpers/measures_helper.rb
@@ -1,9 +1,7 @@
 module MeasuresHelper
 
-  def show_new_measure_link_if_admin
-    if current_user.admin?
-      "#{link_to 'Add a new measure', new_measure_path, class: 'btn btn-primary float-right'}".html_safe
-    end
+  def new_measure_link
+    "#{link_to 'Add a new measure', new_measure_path, class: 'btn btn-primary float-right'}".html_safe
   end
 
 end
