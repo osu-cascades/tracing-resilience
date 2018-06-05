6fdbeb464ceecd2a5612445d4b7072756c0665bb
Nathan Struhs <nathanstruhs@gmail.com>
2018-06-05 11:06:53 -0700

    Fixed #39, add recaptcha to registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  prepend_before_action :check_captcha, only: [:create]
+
   private
 
   def sign_up_params
@@ -12,6 +14,14 @@ class RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:first_name, :last_name, :email,
       :organization, :city, :state, :zip, :phone, :password, :password_confirmation,
       :current_password, :terms_of_use)
+  end
+
+  def check_captcha
+    unless verify_recaptcha
+      self.resource = User.new sign_up_params
+      resource.validate
+      respond_with_navigational(resource) { render :new }
+    end
   end
 
 end
