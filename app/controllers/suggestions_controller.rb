91b85887bfcd538977633748166e5f16f860ad8e
Nathan Struhs <nathanstruhs@gmail.com>
2018-06-05 12:16:30 -0700

    Fixed #40, recaptcha for suggestions form

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -6,8 +6,13 @@ class SuggestionsController < ApplicationController
   def new; end
 
   def create
-    SuggestionMailer.suggestion(current_user, suggestion_params).deliver
-    redirect_to root_path, notice: 'Thank you, your suggestion has been sent.'
+    if verify_recaptcha
+      SuggestionMailer.suggestion(current_user, suggestion_params).deliver
+      redirect_to root_path, notice: 'Thank you, your suggestion has been sent.'
+    else
+      flash[:recaptcha_error]
+      render :new
+    end
   end
 
   private
