2856c225a7ec211da23691065dbd62ff06c168f8
Nathan Struhs <nathanstruhs@gmail.com>
2018-06-11 16:11:38 -0700

    Test measure management

---
+++ spec/support/system_helpers.rb
@@ -0,0 +1,15 @@
+module SystemHelpers
+
+  def create_new_measure(category: 'General', featured: false)
+    visit new_measure_path
+    fill_in('Title', with: 'Example')
+    select('General', from: 'Category')
+    page.attach_file('Document', Rails.root + 'spec/factories/attachments/example.pdf')
+    if featured then check('Featured?') end
+    click_on('Save')
+  end
+
+  def featured_measure_count
+    Measure.all.count { |m| m.featured == true }
+  end
+end
--- spec/system/measure_management_spec.rb
+++ spec/system/measure_management_spec.rb
@@ -3,9 +3,82 @@ require 'rails_helper'
 RSpec.describe 'Measure management' do
   include Devise::Test::IntegrationHelpers
 
-  skip "needs done" do
+  let(:admin) { create(:admin) }
+  let(:measure) { create(:measure) }
+  let(:featured_measure) { build(:featured_measure) }
 
+  before do
+    sign_in(admin)
   end
 
+  context 'admin creates new measure' do
+
+    before do
+      visit new_measure_path
+    end
+
+    scenario 'providing valid measure attributes' do
+      fill_in('Title', with: 'Example title')
+      select('General', from: 'Category')
+      page.attach_file('Document', Rails.root + 'spec/factories/attachments/example.pdf')
+      click_on('Save')
+      expect(page).to have_content('Measure was successfully created.')
+    end
+
+    scenario 'providing invalid measure attributes' do
+      click_on('Save')
+      expect(page).to have_selector '#error_explanation', text: '2 errors prohibited this measure from being saved:'
+      page.find('#error_explanation').tap do |error_explanations|
+        expect(error_explanations).to have_content("Title can't be blank")
+        expect(error_explanations).to have_content("Category can't be blank")
+      end
+    end
+  end
+
+  context 'admin edits measure' do
+
+    before do
+      visit edit_measure_path(measure)
+    end
+
+    scenario 'providing valid measure attributes' do
+      fill_in('Title', with: 'New title')
+      select('Community', from: 'Category')
+      click_on('Save')
+      expect(page).to have_content('Measure was successfully updated.')
+    end
+
+    scenario 'providing invalid measure attributes' do
+      fill_in('Title', with: '')
+      select('Choose...', from: 'Category')
+      click_on('Save')
+      expect(page).to have_selector '#error_explanation', text: '2 errors prohibited this measure from being saved:'
+      page.find('#error_explanation').tap do |error_explanations|
+        expect(error_explanations).to have_content("Title can't be blank")
+        expect(error_explanations).to have_content("Category can't be blank")
+      end
+    end
+  end
+
+  context 'with an existing featured measure' do
+
+    before do
+      visit new_measure_path
+      create_new_measure(category: 'Relational', featured: true)
+    end
+
+    scenario 'admin sets new featured measure' do
+      expect(Measure.count).to equal(1)
+      expect(featured_measure_count).to equal(1)
+      expect(Measure.first.featured).to equal(true)
+
+      create_new_measure(category: 'Individual', featured: true)
+      expect(Measure.count).to equal(2)
+      expect(featured_measure_count).to equal(1)
+      expect(Measure.first.featured).to equal(false)
+      expect(Measure.last.featured).to equal(true)
+    end
+
+  end
 
 end
