264f37e3ff18595ed0259843dd49db75da3df5df
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-01-10 23:57:15 -0800

    Mailer: Update default from and suggestion destination email.

--- app/mailers/suggestion_mailer.rb
+++ app/mailers/suggestion_mailer.rb
@@ -1,11 +1,13 @@
 class SuggestionMailer < ApplicationMailer
 
-  default from: 'tracing-resilience@noreply.com'
+  default from: 'no-reply@tracesco.org'
 
   def suggestion(user, suggestion_params)
     @user = user
     @suggestion = suggestion_params
-    mail to: 'nathanstruhs@gmail.com', subject: "TRACES Resilience Measure Suggestion"
+    mail(to: ENV['SUGGESTIONS_EMAIL_ADDRESS'],
+      subject: 'TRACES Resilience Measure Suggestion'
+    )
   end
 
 end
