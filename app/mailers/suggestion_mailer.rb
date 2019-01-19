0fe07d22f3f707220b1079f743f5e50b530769e8
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-01-19 13:53:25 -0800

    SuggestionMailer: Extract to address and change redirect after sending.
    References #42, #55.

--- app/mailers/suggestion_mailer.rb
+++ app/mailers/suggestion_mailer.rb
@@ -1,11 +1,12 @@
 class SuggestionMailer < ApplicationMailer
 
   default from: 'no-reply@tracesco.org'
+  SUGGESTION_RECIPIENT_EMAIL = ENV['SUGGESTION_RECIPIENT_EMAIL']
 
   def suggestion(user, suggestion_params)
     @user = user
     @suggestion = suggestion_params
-    mail(to: ENV['SUGGESTION_RECIPIENT_EMAIL'],
+    mail(to: SUGGESTION_RECIPIENT_EMAIL,
       subject: 'TRACES Resilience Measure Suggestion'
     )
   end
