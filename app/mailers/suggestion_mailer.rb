669991132f6b66ed698a0b3a9f1a3ea3afd429fc
Ethan Jones <rukokarasu@gmail.com>
2019-01-19 13:35:12 -0800

    Change mailto environment variable to match .env

--- app/mailers/suggestion_mailer.rb
+++ app/mailers/suggestion_mailer.rb
@@ -5,7 +5,7 @@ class SuggestionMailer < ApplicationMailer
   def suggestion(user, suggestion_params)
     @user = user
     @suggestion = suggestion_params
-    mail(to: ENV['SUGGESTIONS_EMAIL_ADDRESS'],
+    mail(to: ENV['SUGGESTION_RECIPIENT_EMAIL'],
       subject: 'TRACES Resilience Measure Suggestion'
     )
   end
